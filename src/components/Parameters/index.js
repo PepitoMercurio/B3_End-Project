23166a87a54454bbe67bd915429ba444b6bb7165
Ibtissam78 <96179116+Ibtissam78@users.noreply.github.com>
2024-05-07 11:13:59 +0200

    ab

--- src/components/Parameters/index.js
+++ src/components/Parameters/index.js
@@ -1,14 +1,50 @@
+import React, { useState } from 'react';
 import { ParameterContainer } from "./style";
 import { Title } from "../Title";
 import { Line } from "../Line";
+import { MdFormatBold } from "react-icons/md";
 
 const Parameters = () => {
-    return (
-        <ParameterContainer>
-            <Title>Design</Title>
-            <Line />
-        </ParameterContainer>
-    );
+  const [size, setSize] = useState(1);
+  const [isBold, setIsBold] = useState(false);
+
+  const handleSizeChange = (event) => {
+    setSize(event.target.value);
+  };
+
+  const handleBoldClick = () => {
+    setIsBold(!isBold);
+  };
+
+  return (
+    <ParameterContainer>
+      <Title>Design</Title>
+      <Line />
+      <label htmlFor="size">Taille:</label>
+      <input type="color" value="#ff0000" />
+
+      <input
+        type="text"
+        id="sizeW"
+        name="W"
+        value={size}
+        onChange={handleSizeChange}
+      />
+      <p>{size}</p>
+      <p>H</p>
+      <input
+        type="text"
+        id="sizeH"
+        name="H"
+        value={size}
+        onChange={handleSizeChange}
+      />
+      <p>{size}</p>
+      <button onClick={handleBoldClick}>
+        <MdFormatBold />
+      </button>
+    </ParameterContainer>
+  );
 }
 
 export default Parameters;
