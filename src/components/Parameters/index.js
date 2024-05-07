5445a16fd85cb4f72b6a8d516b65ff31193f2661
Ibtissam78 <96179116+Ibtissam78@users.noreply.github.com>
2024-05-07 14:20:08 +0200

    abc

--- src/components/Parameters/index.js
+++ src/components/Parameters/index.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { ParameterContainer } from "./style";
 import { Title } from "../Title";
 import { Line } from "../Line";
 import { MdFormatBold } from "react-icons/md";
+import { MdFormatAlignCenter } from "react-icons/md";
+
+const SizeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 6px;
+
+  p {
+    margin-right: 6px;
+  }
+`;
 
 const Parameters = () => {
   const [size, setSize] = useState(1);
   const [isBold, setIsBold] = useState(false);
+  const [isCentered, setIsCentered] = useState(false);
 
   const handleSizeChange = (event) => {
     setSize(event.target.value);
@@ -16,32 +29,47 @@ const Parameters = () => {
     setIsBold(!isBold);
   };
 
+  const handleCenterClick = () => {
+    setIsCentered(!isCentered);
+  };
+
   return (
     <ParameterContainer>
       <Title>Design</Title>
       <Line />
       <label htmlFor="size">Taille:</label>
+      <SizeContainer>
+        <p>W</p>
+        <input
+          type="text"
+          id="sizeW"
+          name="W"
+          value={size}
+          onChange={handleSizeChange}
+          style={{ width: '50px', height: '20px' }}
+        />
+        <p>{size}</p>
+      </SizeContainer>
+      <SizeContainer>
+        <p>H</p>
+        <input
+          type="text"
+          id="sizeH"
+          name="H"
+          value={size}
+          onChange={handleSizeChange}
+          style={{ width: '50px', height: '20px' }}
+        />
+        <p>{size}</p>
+
+      </SizeContainer>
       <input type="color" value="#ff0000" />
 
-      <input
-        type="text"
-        id="sizeW"
-        name="W"
-        value={size}
-        onChange={handleSizeChange}
-      />
-      <p>{size}</p>
-      <p>H</p>
-      <input
-        type="text"
-        id="sizeH"
-        name="H"
-        value={size}
-        onChange={handleSizeChange}
-      />
-      <p>{size}</p>
       <button onClick={handleBoldClick}>
         <MdFormatBold />
+      </button>
+      <button onClick={handleCenterClick}>
+        <MdFormatAlignCenter />
       </button>
     </ParameterContainer>
   );
